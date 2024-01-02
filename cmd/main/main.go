8ce8ada991902223e907c16215c53c478da04598
Tomas Climente <tomas.climente@kavak.com>
2024-01-02 12:01:53 -0300

    clean

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,108 +1,82 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/chi/v5"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"strings"
+	"net/http/httputil"
+	"net/url"
 	"time"
-
-	"github.com/gorilla/mux"
 )
-
-const (
-	servicePort string = `:8080`
-	proxyPort   string = `:8081`
-)
-
-func getHttpClient() *http.Client {
-	transport := http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		Dial: (&net.Dialer{
-			// Modify the time to wait for a connection to establish
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-		}).Dial,
-		ForceAttemptHTTP2:     true,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-	}
-
-	client := http.Client{
-		Transport: &transport,
-		Timeout:   3 * time.Second,
-	}
-
-	return &client
-}
-
-func getTargetUrl(url string) string {
-	urlParts := strings.Split(url, `/`)
-
-	// return urlParts[1] + servicePort + `/` + strings.Join(urlParts[2:], `/`)
-
-	return fmt.Sprintf("%s://%s%s", "http", urlParts[1]+servicePort+"/", strings.Join(urlParts[2:], `/`))
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// Create a new HTTP request with the same method, URL, and body as the original request
-	targetURL := r.URL.String()
-	serviceEndpoint := getTargetUrl(targetURL)
-	proxyReq, err := http.NewRequest(r.Method, serviceEndpoint, r.Body)
-	if err != nil {
-		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
-		return
-	}
-
-	// Copy the headers from the original request to the proxy request
-	for name, values := range r.Header {
-		for _, value := range values {
-			proxyReq.Header.Add(name, value)
-		}
-	}
-
-	// Send the proxy request using the custom transport
-	resp, err := getHttpClient().Transport.RoundTrip(proxyReq)
-	if err != nil {
-		fmt.Println(err)
-		http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
-	// Copy the headers from the proxy response to the original response
-	for name, values := range resp.Header {
-		for _, value := range values {
-			w.Header().Add(name, value)
-		}
-	}
-
-	// Set the status code of the original response to the status code of the proxy response
-	w.WriteHeader(resp.StatusCode)
-
-	// Copy the body of the proxy response to the original response
-	io.Copy(w, resp.Body)
-}
 
 func main() {
-	router := mux.NewRouter()
+	r := chi.NewRouter()
 
-	router.PathPrefix("/").Handler(http.HandlerFunc(handleRequest))
+	r.HandleFunc("/*", func(res http.ResponseWriter, req *http.Request) {
+		path := req.URL.Path
+		qs := req.URL.Query()
+		body := req.Body
+		log.Printf("Incoming request: %s", req.URL.String())
+		targetURL, isPublic := lookupTargetURL(path, qs, body)
+		if targetURL == "" {
+			http.Error(res, "Not Found", 404)
+			return
+		}
+		if !isPublic {
+			//if (!myAuth.authenticate(req.Header.Get("Authorization"))) {
+			http.Error(res, "Unauthorized", 401)
+			return
+			//}
+		}
+		client := &http.Client{Timeout: 10 * time.Second}
+		proxy(client, targetURL, res, req)
+	})
 
-	// Create a new HTTP server with the handleRequest function as the handler
-	server := http.Server{
-		Addr:    proxyPort,
-		Handler: router,
-	}
+	log.Println("El gateway está corriendo en el puerto 8080")
+	http.ListenAndServe(":8080", r)
+}
 
-	// Start the server and log any errors
-	log.Println("Starting proxy server on " + proxyPort)
-	err := server.ListenAndServe()
+func lookupTargetURL(path string, qs url.Values, body io.ReadCloser) (string, bool) {
+	log.Println("WEAS")
+	log.Println(path)
+	log.Println(qs)
+
+	bodyBytes, err := ioutil.ReadAll(body)
+	defer body.Close()
 	if err != nil {
-		log.Fatal("Error starting proxy server: ", err)
+		return "", false
 	}
+
+	bodyString := string(bodyBytes)
+	log.Println(bodyString)
+
+	return "http://localhost:3000/ping", true
+}
+
+func proxy(client *http.Client, targetURL string, w http.ResponseWriter, r *http.Request) {
+	url, err := url.Parse(targetURL)
+	if err != nil {
+		http.Error(w, "Invalid URL", 500)
+		return
+	}
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.Director = func(req *http.Request) {
+		for key, value := range req.Header {
+			req.Header[key] = value
+		}
+		req.Header.Add("my-header", "value")
+
+		req.URL.Scheme = url.Scheme
+		req.URL.Host = url.Host
+		req.URL.Path = url.Path
+	}
+	proxy.Transport = &http.Transport{
+		DialContext:           (&net.Dialer{Timeout: client.Timeout}).DialContext,
+		ResponseHeaderTimeout: client.Timeout,
+		ExpectContinueTimeout: client.Timeout,
+	}
+	proxy.ServeHTTP(w, r)
 }
